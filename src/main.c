7406b1e877b9da787975f8e8cbe6e80203e0943e
draghi <draghi@MetaXubuntu-Draghi>
2015-03-06 18:30:16 +0800

    Added mouse camera control and added teapot wireframe, also added cubes.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "glut/Display.h"
+#include "glut/Mouse.h"
 #include"glut/GlutCallbacks.h"
 #include <GL/glut.h>
 
@@ -10,6 +11,9 @@ GLfloat colors[][3] = {{0.0,0.0,0.0},{1.0,0.0,0.0},{1.0,1.0,0.0},{0.0,1.0,0.0},
 
 /**@todo Remove testing code.*/
 float rot = 0;
+float camRotX = 45;
+float camRotY = 0;
+float dist = 15.0;
 
 /**@todo Remove testing code.*/
 static void polygon(int a, int b, int c , int d) {
@@ -40,22 +44,35 @@ static void render() {
 	 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	 glLoadIdentity();
-	 	 int i = 0;
-	 	 int j = 0;
-	 	 for(i=0; i<3; i++) {
-		 	 for(j=0; j<3; j++) {
-				 glPushMatrix();
-				 	 //"Camera"
-				 	 glTranslatef(0.0, -4.0, 0.0);
-				 	 glRotatef(45, 1.0, 0.0, 0.0);
+	 glPushMatrix();
+ 	 	 //"Camera"
+ 	 	 glRotatef(-90, 0.0, 1.0, 0.0);
+	 	 glTranslatef(-dist, 0.0, 0.0);
+ 	 	 glRotatef(camRotX, 0.0, 0.0, -1.0);
+ 	 	 glRotatef(camRotY, 0.0, 1.0, 0.0);
+ 	 	 glTranslatef(0.0, -2.0, 0.0);
 
-				 	 //"Model"
-					 glTranslatef(i*3-3, -2.0, -(5.75+j*3));
-					 glRotatef(rot, 0.0, 1.0, 0.0);
-					 colorcube();
-				 glPopMatrix();
-		 	 }
-	 	 }
+ 	 	 int size = 50;
+
+			 int i = 0;
+			 int j = 0;
+			 for(i=0; i<size; i++) {
+				 for(j=0; j<size; j++) {
+					 glPushMatrix();
+						 //"Model"
+						 glTranslatef(i*4-(size/2*4-1.5), -2.0, -(j*4-(size/2*4-1.5)));
+						 glRotatef(rot, 0.0, 1.0, 0.0);
+						 colorcube();
+					 glPopMatrix();
+				 }
+			 }
+
+ 		 glPushMatrix();
+			 glRotatef(-rot/10, 0.0, 1.0, 0.0);
+			 glTranslatef(0, 5.0, 0);
+	 	 	 glutWireTeapot(5);
+		 glPopMatrix();
+	 glPopMatrix();
 	 glFlush();
 
 	 glutSwapBuffers();
@@ -64,9 +81,17 @@ static void render() {
 /**@todo Remove testing code.*/
 static void update() {
 	rot++;
-
-	if (rot>=360)
+	if (rot>=3600)
 		rot = 0;
+
+	if (mouse.isButtonDown(GLUT_LEFT_BUTTON)) {
+		camRotX += mouse.getYMove();
+		camRotY += mouse.getXMove();
+	}
+
+	dist -= mouse.getScrollCount();
+	if (dist<0)
+		dist=0;
 }
 
 /**@todo Remove testing code.*/
@@ -75,7 +100,7 @@ static void onResize(int32_t w, int32_t h) {
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(60,w/(float)h,0.1,100);
+    gluPerspective(60,w/(float)h,0.2,200);
 
     glMatrixMode(GL_MODELVIEW);
 }
