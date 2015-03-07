5bea297ad6270bed08577bebd89a6e25195c2d6c
samdelion <samueldavid.ep@gmail.com>
2015-03-07 14:37:36 +0800

    Removed extern keyword from header files (unnecessary)

--- src/math/Vec3.h
+++ src/math/Vec3.h
@@ -15,16 +15,16 @@ typedef struct Vec3_s {
     scalar x, y, z;
 } Vec3;
 
-extern Vec3     createVec3(scalar x, scalar y, scalar z);
-extern Vec3     cloneVec3(const Vec3 *const vec);
+Vec3     createVec3(scalar x, scalar y, scalar z);
+Vec3     cloneVec3(const Vec3 *const vec);
 
-extern Vec3     sumVec3Vec3(const Vec3 *const v1, const Vec3 *const v2);
-extern Vec3     subVec3Vec3(const Vec3 *const v1, const Vec3 *const v2);
-extern Vec3     mulVec3Scalar(const Vec3 *const vec, scalar factor);
-extern Vec3     mulScalarVec3(scalar factor, const Vec3 *const vec);
-extern Vec3     invertVec3(const Vec3 *const vec);
-extern scalar   dotVec3(const Vec3 *const v1, const Vec3 *const v2);
-extern Vec3     crossVec3(const Vec3 *const v1, const Vec3 *const v2);
-extern Vec3     normalizeVec3(const Vec3 *const vec);
+Vec3     sumVec3Vec3(const Vec3 *const v1, const Vec3 *const v2);
+Vec3     subVec3Vec3(const Vec3 *const v1, const Vec3 *const v2);
+Vec3     mulVec3Scalar(const Vec3 *const vec, scalar factor);
+Vec3     mulScalarVec3(scalar factor, const Vec3 *const vec);
+Vec3     invertVec3(const Vec3 *const vec);
+scalar   dotVec3(const Vec3 *const v1, const Vec3 *const v2);
+Vec3     crossVec3(const Vec3 *const v1, const Vec3 *const v2);
+Vec3     normalizeVec3(const Vec3 *const vec);
 
 #endif
