cac1aec7ac6fb47cff6e60b2a28f903baf02d1ce
draghi <draghi@MetaXubuntu-Draghi>
2015-03-09 00:12:53 +0800

    Added pretty much everything related to graphics into the header.

--- src/graphics.h
+++ src/graphics.h
@@ -3,8 +3,29 @@
 
 #define GLEW_STATIC
 
+/////////////////////////
+// 3rd Party Libraries //
+/////////////////////////
 #include "lib/glew/glew.h"
 #include <GL/freeglut.h>
+
+////////////////////
+// Debugging Tools //
+////////////////////
 #include "debug/GLDebugUtil.h"
+
+//////////////////
+// Helper Utils //
+//////////////////
+#include "glut/Display.h"
+#include "glut/Mouse.h"
+#include "glut/Keyboard.h"
+#include "gl/GLUtil.h"
+
+///////////////////
+// Graphics Libs //
+///////////////////
+#include "gl/Textures.h"
+#include "gl/VBO.h"
 
 #endif /* SRC_OGL_H_ */
