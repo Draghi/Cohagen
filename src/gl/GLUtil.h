fc7d7de925ad600e7c30a99462c10c5b2d17abf3
draghi <draghi@MetaXubuntu-Draghi>
2015-03-16 20:54:39 +0800

    Merged GLUtil and GLDebugUtil into glUtil and added comments.

--- src/gl/GLUtil.h
+++ src/gl/GLUtil.h
@@ -16,6 +16,19 @@
  * @param windingDir The direction that the polygon winds in (GL_CW/ GL_CCE)
  */
 void glSetBackfaceCulling(const GLenum windingDir);
+
+/**
+ * Attempts to return the size in whole bytes of a pixel of a given type.
+ * (GL_RGB5 will be 2 bytes, despite only using 1.5 bytes)
+ * @param type The type to attempt to work out the size of.
+ * @return The size of the data type in whole bytes.
+ */
 uint32_t glGetPixelSize(const GLenum type);
+
+/**
+ * Checks the OpenGL error state and prints messages.
+ * @return Whether an error was returned.
+ */
+bool glCheckError();
 
 #endif /* GL_GLUTIL_H_ */
