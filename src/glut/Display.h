effb52c4aa6eed704d1a40a4873ad3151f5fc7c7
draghi <draghi@MetaXubuntu-Draghi>
2015-03-31 15:46:20 +0800

    Added differing TPS/FPS ability, based on virtual tps and fps. Ensures deterministic behavour.

--- src/glut/Display.h
+++ src/glut/Display.h
@@ -9,162 +9,171 @@
  * Do not touch this, unless you're sure of what you're doing.
  */
 struct Display_s {
-
 	/**
-	 * Creates a new window from stored variable.
+	 * Creates a glut window from the settings provided by the display.
+	 * There may only be one display at any one given time.
 	 *
-	 * @param pargc a pointer to the argc main() variable.
-	 * @param argv a pointer to the argv main() variable.
-	 * @return Whether or not the window creation was attempted (Can't create twice)
+	 * @return If a new window was created or not.
 	 */
 	bool(* createWindow)();
 
 	/**
-	 * Attempts to center the window on the screen.
+	 * Helper method to center the window in one call.
 	 */
 	void(* doCenterWindow)();
 
 	/**
-	 * Sets the major and minor version of the requested openGL context.
-	 * Cannot be changed after window creation.
+	 * Changes the target OpenGL major and minor versions.
 	 *
-	 * @param major The major version number of OpenGL (OpenGL x.0 eg.)
-	 * @param minor The minor version number of OpenGL (OpenGL 0.x eg.)
+	 * @param major The major version number of OpenGL (OpenGL x.0)
+	 * @param minor The minor version number of OpenGL (OpenGL 0.x)
 	 */
-	void(*setOGLVersion)(const uint8_t, const uint8_t);
+	void(*setOGLVersion)(const uint8_t major, const uint8_t minor);
 
 	/**
-	 * Sets the display mode of the window (Doublebuffer, multisampling etc.)
-	 * Cannot be changed once the window is created.
+	 * Sets the glut display mode.
+	 * Only functions prior to window creation.
 	 *
-	 * @param displayMode The bitmasked glut display mode.
+	 * @param dispMode The display mode to set glut to.
 	 */
-	void(*setDisplayMode)(const uint32_t);
+	void(*setDisplayMode)(const uint32_t dispMode);
 
 	/**
-	 * Changes the position of the window.
+	 * Changes the x and y position of the window.
 	 *
-	 * @param x The new x-position of the window.
-	 * @param y The new y-position of the window.
+	 * @param x The new x position of the window.
+	 * @param y The new y position of the window.
 	 */
-	void(*setWindowPos)(const int32_t, const int32_t);
+	void(*setWindowPos)(const int32_t x, const int32_t y);
 
 	/**
-	 * Changes the x-position of the window.
+	 * Changes the x position of the window.
 	 *
 	 * @param x The new x position of the window.
 	 */
-	void(*setWindowX)(const int32_t);
+	void(*setWindowX)(const int32_t x);
 
 	/**
-	 * Changes the y-position of the window.
+	 * Changes the y position of the window.
 	 *
 	 * @param y The new y position of the window.
 	 */
 	void(*setWindowY)(const int32_t);
 
 	/**
-	 * Changes the size of the window.
-	 *
+	 * Changes the width and height of the window
 	 * @param width The new width of the window.
 	 * @param height The new height of the window.
 	 */
-	void(*setWindowSize)(const int32_t, const int32_t);
+	void(*setWindowSize)(const int32_t width, const int32_t height);
 
 	/**
-	 * Changes the width of window.
-	 *
+	 * Changes the width of the window
 	 * @param width The new width of the window.
 	 */
-	void(*setWindowWidth)(const int32_t);
+	void(*setWindowWidth)(const int32_t width);
 
 	/**
-	 * Changes the height of the window.
-	 *
+	 * Changes the height of the window
 	 * @param height The new height of the window.
 	 */
-	void(*setWindowHeight)(const int32_t);
+	void(*setWindowHeight)(const int32_t height);
 
 	/**
-	 * Changes the window's title.
-	 *
+	 * Changes the title of the window.
 	 * @param title The new title of the window.
 	 */
 	void(*setWindowTitle)(const char[]);
 
 	/**
-	 * Sets the render callback. Called when the screen should be updated.
-	 *
-	 * @param callbacks The GlutMainCallbacks containing the callbacks.
+	 * Sets the callback to be used when the window is redrawn.
+	 * @param callback The callback to be used when the window is redrawn.
 	 */
-	void(*setRenderCallback)(void(* callback)());
+	void(*setRenderCallback)(void(* callback)(uint32_t));
 
 	/**
-	 * Sets the update callback. Called when the game's state should be updated.
-	 *
-	 * @param callbacks The GlutWindowCallbacks
+	 * Sets the callback to be used when the world should be updated.
+	 * @param callback The callback to be used when the world should be updated.
 	 */
-	void(*setUpdateCallback)(void(* callback)());
+	void(*setUpdateCallback)(void(* callback)(uint32_t));
 
 	/**
-	 * Returns the OpenGL major version (OpenGL x.3 eg.)
-	 * @return The OpenGL major version
+	 * Sets the virtual TPS target.
+	 * @param tps The target TPS
+	 */
+	void(* setVirtualTPS)(float tps);
+
+	/**
+	 * Sets the virtual FPS target.
+	 * @param fps The target FPS
+	 */
+	void(* setVirtualFPS)(float fps);
+
+	/**
+	 * Returns the major openGL version of the context.
+	 * @return The major openGL version of the context.
 	 */
 	int8_t (*getOGLMajorVer)();
 
 	/**
-	 * Returns the OpenGL minor version (OpenGL 3.x eg.)
-	 * @return The OpenGL minor version
+	 * Returns the minor openGL version of the context.
+	 * @return The minor openGL version of the context.
 	 */
 	int8_t (*getOGLMinorVer)();
 
 	/**
-	 * Returns the window's height
-	 * @return The window's height
+	 * Returns the window height
+	 * @return The window height
 	 */
 	int32_t (*getWindowHeight)();
 
 	/**
-	 * Returns the window's width.
-	 * @return The window's width
+	 * Returns the window width
+	 * @return The window width
 	 */
 	int32_t (*getWindowWidth)();
 
 	/**
-	 * Returns the window's x-position.
-	 * @return The window's x-position
+	 * Returns the window x pos
+	 * @return The window x pos
 	 */
 	int32_t (*getWindowX)();
 
 	/**
-	 * Returns the windows y-position.
-	 * @return The windows y-position
+	 * Returns the window y pos
+	 * @return The window y pos
 	 */
 	int32_t (*getWindowY)();
 
 	/**
-	 * Returns the window's title.
-	 * @return The window's title
+	 * Returns the window title
+	 * @return The window title
 	 */
 	const char* (*getWindowTitle)();
 
 	/**
-	 * Returns the screen's width.
-	 * @return The screen's width.
+	 * Returns the screen width
+	 * @return The screen width
 	 */
 	int32_t(* getScreenWidth)();
 
 	/**
-	 * Returns the screen's height.
-	 * @return The screen's height.
+	 * Returns the screen height
+	 * @return The screen height
 	 */
 	int32_t(* getScreenHeight)();
 
 	/**
-	 * Returns the FPS.
-	 * @return The FPS.
+	 * Returns the current fps
+	 * @return The current fps
 	 */
 	int32_t(* getFPS)();
+
+	/**
+	 * Returns the current tps
+	 * @return The current tps
+	 */
+	int32_t(* getTPS)();
 };
 
 /**
