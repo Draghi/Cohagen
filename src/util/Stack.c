bc8063f8b77127b51f30feb527b3b558155e6e36
draghi <draghi@MetaXubuntu-Draghi>
2015-05-04 12:38:26 +0800

    Fixed major bugs in the matrix mananger. Completely useable now! Woo!

--- src/util/Stack.c
+++ src/util/Stack.c
@@ -14,20 +14,28 @@ static void changeSize(Stack* stack, uint64_t size) {
 static void set(Stack* stack, void* data, uint64_t pos) {
 	if (pos>=(stack->arraySize)) {
 		changeSize(stack, pos+1);
+	} else {
+		if(stack->contents[pos]!=NULL)
+			free(stack->contents[pos]);
 	}
 
 	stack->contents[pos] = data;
+
+	if (stack->elementSize < pos+1)
+		stack->elementSize = pos+1;
 }
 
 static void remove(Stack* stack, uint64_t pos) {
-	if (pos<(stack->elementSize)) {
+	if (pos<stack->elementSize) {
 		stack->elementSize--;
 
 		if (pos!=(stack->elementSize)) {
-			for(int i = pos; i<(stack->elementSize)--; i++) {
+			for(int i = pos; i<(stack->elementSize); i++) {
 				stack->contents[i] = stack->contents[i+1];
 			}
 		}
+
+		//manStack.trim(stack);
 	}
 }
 
@@ -37,6 +45,7 @@ static void remove(Stack* stack, uint64_t pos) {
 static Stack* new() {
 	Stack* stack = malloc(sizeof(Stack));
 	stack->contents = malloc(sizeof(void**));
+	stack->contents[0] = NULL;
 	stack->arraySize = 1;
 	stack->elementSize = 0;
 
@@ -48,7 +57,10 @@ static void push(Stack* stack, void* data) {
 }
 
 static void* peek(Stack* stack) {
-	return stack->contents[stack->elementSize-1];
+	if (stack->elementSize>0)
+		return stack->contents[stack->elementSize-1];
+
+	return NULL;
 }
 
 static void* pop(Stack* stack) {
