d26049ea2c8e122efab304cb20c1f1a9224d01a4
draghi <draghi@MetaXubuntu-Draghi>
2015-05-17 08:49:37 +0800

    Added defines for the min and max represtanable values for the given precision.

--- src/math/Precision.h
+++ src/math/Precision.h
@@ -1,6 +1,11 @@
 #ifndef MATH_PRECISION_H
 #define MATH_PRECISION_H
 
+#include <float.h>
+
+#define SCALAR_MAX_VAL FLT_MAX
+#define SCALAR_MIN_VAL -FLT_MAX
+
 /**
  *  Used in math classes, allows us to easily change the precision
  *  of our physics calculations.
