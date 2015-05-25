63621c37bde79509f9455e24977692881447415c
Kieron van der Kwast <vdk.kieron@gmail.com>
2015-05-25 20:29:14 +0800

    Another small change, added setter and getter declarations to Viewport.h, along with doxygen comments.

--- src/gl/Viewport.h
+++ src/gl/Viewport.h
@@ -22,6 +22,65 @@ typedef struct ViewportManager_s {
 	 */
 	void (*makeActive)(const Viewport *const);
 
+	/**
+	 * Getter for x
+	 *
+	 * @returns x
+	 */
+	float (*GetX)();
+
+	/**
+	 * Setter for x
+	 *
+	 * @param newX		float value to set x
+	 * @returns void
+	 */
+	void (*SetX)(float newX);
+
+	/**
+	 * Getter for y
+	 *
+	 * @returns y
+	 */
+	float (*GetY)();
+
+	/**
+	 * Setter for y
+	 *
+	 * @param newY		float value to set y
+	 * @returns void
+	 */
+	void (*SetY)(float newY);
+
+	/**
+	 * Getter for height
+	 *
+	 * @returns height
+	 */
+	float (*GetHeight)();
+
+	/**
+	 * Setter for height
+	 *
+	 * @param newHeight		float value to set height
+	 * @returns void
+	 */
+	void (*SetHeight)(float newHeight);
+
+	/**
+	 * Getter for width
+	 *
+	 * @returns width
+	 */
+	float (*GetWidth)();
+
+	/**
+	 * Setter for width
+	 *
+	 * @param newWidth		float value to set width
+	 * @returns void
+	 */
+	void (*SetWidth)(float newWidth);
 
 } ViewportManager;
 
