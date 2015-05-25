85a511ef3352f91a5f920ff825a73ed1cba07867
Kieron van der Kwast <vdk.kieron@gmail.com>
2015-05-25 18:42:30 +0800

    Added the header file Viewport.h, for the Viewport class.

--- src/gl/Viewport.h
+++ src/gl/Viewport.h
@@ -1,14 +1,27 @@
 #ifndef COH_VIEWPORT_H
 #define COH_VIEWPORT_H
 
+/**
+ * Viewport Object
+ */
 typedef struct Viewport_s {
+
 	float x;
 	float y;
 	float width;
 	float height;
+
 } Viewport;
 
 typedef struct ViewportManager_s {
+
+	/**
+	 * Makes a call to the function glViewport
+	 *
+	 * @param viewport pointer to Viewport object being bound
+	 */
+	void (*makeActive)(const Viewport *const);
+
 
 } ViewportManager;
 
