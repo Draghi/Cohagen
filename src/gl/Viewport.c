71674c038750b5b7a3220e02f1da90f2167a2b93
Kieron van der Kwast <vdk.kieron@gmail.com>
2015-05-26 17:40:17 +0800

    Fixed several issues. Function names incorrectly named in Viewport class have been correctly named. glUniform functions in Shader class are implemented. Parameters have been added in prototypes and in declarations for Shader class and Viewport class.

--- src/gl/Viewport.c
+++ src/gl/Viewport.c
@@ -4,14 +4,14 @@
 #include "util/OGLUtil.h"
 
 static void makeActive(const Viewport *const viewport);
-static float GetX();
-static void SetX(float newX);
-static float GetY();
-static void SetY(float newY);
-static float GetHeight();
-static void SetHeight(float newHeight);
-static float GetWidth();
-static void SetWidth(float newWidth);
+static float getX(const Viewport *const viewport);
+static void setX(Viewport *const viewport, float newX);
+static float getY(const Viewport *const viewport);
+static void setY(Viewport *const viewport, float newY);
+static float getHeight(const Viewport *const viewport);
+static void setHeight(Viewport *const viewport, float newHeight);
+static float getWidth(const Viewport *const viewport);
+static void setWidth(Viewport *const viewport, float newWidth);
 
 
 static void makeActive(const Viewport *const viewport){
@@ -19,42 +19,42 @@ static void makeActive(const Viewport *const viewport){
 }
 
 
-static float GetX(const Viewport *const viewport){
+static float getX(const Viewport *const viewport){
 	return viewport->x;
 }
 
 
-static void SetX(const Viewport *const viewport, float newX){
+static void setX(Viewport *const viewport, float newX){
 	viewport->x = newX;
 }
 
 
-static float GetY(const Viewport *const viewport){
+static float getY(const Viewport *const viewport){
 	return viewport->y;
 }
 
 
-static void SetY(const Viewport *const viewport, float newY){
+static void setY(Viewport *const viewport, float newY){
 	viewport->y = newY;
 }
 
 
-static float GetHeight(const Viewport *const viewport){
+static float getHeight(const Viewport *const viewport){
 	return viewport->height;
 }
 
 
-static void SetHeight(const Viewport *const viewport, float newHeight){
+static void setHeight(Viewport *const viewport, float newHeight){
 	viewport->height = newHeight;
 }
 
 
-static float GetWidth(const Viewport *const viewport){
+static float getWidth(const Viewport *const viewport){
 	return viewport->width;
 }
 
 
-static void SetWidth(const Viewport *const viewport, float newWidth){
+static void setWidth(Viewport *const viewport, float newWidth){
 	viewport->width = newWidth;
 }
 
