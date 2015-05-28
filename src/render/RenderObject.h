43949bb027eafdeddd5520d82fa261f44c0884b2
draghi <draghi@MetaXubuntu-Draghi>
2015-05-28 12:43:41 +0800

    Some doco! Woo!

--- src/render/RenderObject.h
+++ src/render/RenderObject.h
@@ -7,18 +7,51 @@
 #include "util/DynamicArray.h"
 
 typedef struct RenderObject_s {
+	/** Pointer to the position to use. **/
 	Vec3* position;
+	/** Pointer to the rotation to use. **/
 	Vec3* rotation;
+	/** Pointer to the scale to use.**/
 	Vec3* scale;
 
+	/** Poiner to the model to use **/
 	VAO* model;
+
+	/** A dynamic array of textures. **/
 	DynamicArray* textures;
 } RenderObject;
 
 typedef struct RenderObjectManager_s {
+	/**
+	 * Creates a new renderobject.
+	 * @remark The value of the pointer is store (The render object will reference them)
+	 * @param position The pointer to the position to use. NULL will allocate a new position.
+	 * @param rotation The pointer to the rotation to use. NULL will allocate a new rotation.
+	 * @param scale The pointer to the scale to use. NULL will allocate a new scale.
+	 * @return A new RenderObject.
+	 */
 	RenderObject*(* new)(Vec3 *position, Vec3* rotation, Vec3* scale);
+
+	/**
+	 * Changes the VAO of the given render object.
+	 * @param renderObject The RenderObject to modify.
+	 * @param vao The new VAO to use.
+	 */
 	void(* setModel)(RenderObject* renderObject, VAO* vao);
+
+	/**
+	 * Adds a new texture to the Render Object.
+	 * @param renderObject The RenderObject to modify.
+	 * @param texture The texture to add.
+	 * @remarks The order in which they are added, defines which slot they will use.
+	 */
 	void(* addTexture)(RenderObject* renderObject, Texture* texture);
+
+	/**
+	 * Deletes the renderobject.
+	 * @remark Does not free position, rotation, scale or VAO. That is up to the caller.
+	 * @param renderObject The RenderObject to delete.
+	 */
 	void(* delete)(RenderObject* renderObject);
 } RenderObjectManager;
 
--- src/render/Renderer.h
+++ src/render/Renderer.h
@@ -5,15 +5,41 @@
 #include "render/MatrixManager.h"
 #include "gl/Shader.h"
 
+/** The name of the projection matrix to look for in a shader **/
 static const char PROJECTION_MATRIX_NAME[] = "projMat";
+/** The name of the view matrix to look for in a shader. **/
 static const char       VIEW_MATRIX_NAME[] = "viewMat";
+/** The name of the model matrix to look for in a shader. **/
 static const char      MODEL_MATRIX_NAME[] = "modelMat";
+/** The name of the model-View-Projection matrix to look for in a shader. **/
 static const char        MVP_MATRIX_NAME[] = "mvpMat";
 
 typedef struct RendererManager_s {
+	/**
+	 * Applies the transformations contained in the RenderObject to the given MatrixManager
+	 * @param model The model to use the transformations of.
+	 * @param matMan The matrix manager to transform.
+	 */
 	void(* applyTransformations)(RenderObject* model, MatrixManager* matMan);
+	/**
+	 * Binds the textures in the given DynamicArray, in order.
+	 * @remarks Will bind to texture slots 0..length(DynamicArray)-1 in the order of the array.
+	 * @param textures The textures to bind.
+	 */
 	void(* bindTextures)(DynamicArray* textures);
+	/**
+	 * Binds the projection, model and view matrix to the given shader.
+	 * Also calcuates the model-view-projection matrix and binds to the given shader.
+	 * @param shader The shader to bind too.
+	 * @param matMan The matrix manager to get the matrices from.
+	 */
 	void(* bindMatricies)(Shader* shader, MatrixManager* matMan);
+	/**
+	 * Renders the given render object, using the shader and matrix manager.
+	 * @param model The render object to render.
+	 * @param shader The shader to use.
+	 * @param matMan The Matrix Manager to use.
+	 */
 	void(* renderModel)(RenderObject* model, Shader* shader, MatrixManager* matMan);
 } RendererManager;
 
